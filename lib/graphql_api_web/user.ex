e6b09d75f8fc01bd02b1625211db926c848e152b
Gabriel Jardio <gabejardio@gmail.com>
2022-05-02 09:46:44 -0500

    add static list of users for our api

---
+++ lib/graphql_api_web/user.ex
@@ -0,0 +1,40 @@
+defmodule GraphqlApiWeb.User do
+  @users [
+    %{
+      id: 1,
+      name: "Bill",
+      email: "bill@gmail.com",
+      preferences: %{
+        likes_emails: false,
+        likes_phone_calls: true
+      }
+    },
+    %{
+      id: 2,
+      name: "Alice",
+      email: "alice@gmail.com",
+      preferences: %{
+        likes_emails: true,
+        likes_phone_calls: false
+      }
+    },
+    %{
+      id: 3,
+      name: "Jill",
+      email: "jill@hotmail.com",
+      preferences: %{
+        likes_emails: true,
+        likes_phone_calls: true
+      }
+    },
+    %{
+      id: 4,
+      name: "Tim",
+      email: "tim@gmail.com",
+      preferences: %{
+        likes_emails: false,
+        likes_phone_calls: false
+      }
+    }
+  ]
+end
