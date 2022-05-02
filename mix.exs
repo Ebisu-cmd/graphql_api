e064500ba47cd6cb956433bccf9907d920f189f0
Gabriel Jardio <gabejardio@gmail.com>
2022-05-02 09:23:56 -0500

    Added modules: absinthe, absinthe_plug, absinthe_phoenix

--- mix.exs
+++ mix.exs
@@ -37,7 +37,10 @@ defmodule GraphqlApi.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7.0"},
+      {:absinthe_plug, "~> 1.5.8"},
+      {:absinthe_phoenix, "~> 2.0.2"}
     ]
   end
 
