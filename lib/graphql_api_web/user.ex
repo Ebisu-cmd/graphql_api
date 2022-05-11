cd1bc2b956a06f103db456d522f1e7970643c5bf
Gabriel Jardio <gabejardio@gmail.com>
2022-05-11 09:15:40 -0500

    Redid filtering for find_users and update_user_preferences

--- lib/graphql_api_web/user.ex
+++ lib/graphql_api_web/user.ex
@@ -50,43 +50,15 @@ defmodule GraphqlApiWeb.User do
   end
 
   def find_users(params) do
-    @users
-    |> filter_users_who_like_emails(params)
-    |> filter_users_who_like_phone_calls(params)
-    |> filter_users_who_like_faxes(params)
-    |> find_users_error_check(params)
-  end
-
-  defp filter_users_who_like_emails(users, params) do
-    if Map.has_key?(params, :likes_emails) do
-      Enum.filter(users, &(&1.preferences.likes_emails === params.likes_emails))
-    else
-      users
-    end
-  end
-
-  defp filter_users_who_like_phone_calls(users, params) do
-    if Map.has_key?(params, :likes_phone_calls) do
-      Enum.filter(users, &(&1.preferences.likes_phone_calls === params.likes_phone_calls))
-    else
-      users
-    end
-  end
-
-  defp filter_users_who_like_faxes(users, params) do
-    if Map.has_key?(params, :likes_faxes) do
-      Enum.filter(users, &(&1.preferences.likes_faxes === params.likes_faxes))
-    else
-      users
-    end
-  end
-
-  defp find_users_error_check(users, params) do
-    case users do
+    filtered_users = Enum.reduce(params, @users, fn {preference, value}, acc ->
+      Enum.filter(acc, fn user -> user.preferences[preference] === value end)
+    end)
+    case filtered_users do
       [] -> {:error, %{message: "Users not found", details: params}}
       users -> {:ok, users}
     end
   end
+
 
   def create_user(params) do
     case find_user(%{id: params.id}) do
@@ -103,38 +75,10 @@ defmodule GraphqlApiWeb.User do
 
   def update_user_preferences(id, params) do
     with {:ok, user} <- find_user(%{id: id}) do
-      user.preferences
-      |> update_users_email_preference(params)
-      |> update_users_phone_preference(params)
-      |> update_users_fax_preference(params)
-      |> add_user_id_for_subscription(user)
+      new_user_preferences = Enum.reduce(params, user.preferences, fn {preference, value}, acc ->
+        Map.put(acc, preference, value)
+      end)
+      {:ok, Map.put(new_user_preferences, :user_id, user.id)}
     end
-  end
-
-  defp update_users_email_preference(user_preferences, params) do
-    %{likes_emails: likes_emails} = params
-    Map.merge(user_preferences, %{likes_emails: likes_emails})
-  end
-
-  defp update_users_phone_preference(user_preferences, params) do
-    if Map.has_key?(params, :likes_phone_calls) do
-      %{likes_phone_calls: likes_phone_calls} = params
-      Map.merge(user_preferences, %{likes_phone_calls: likes_phone_calls})
-    else
-      user_preferences
-    end
-  end
-
-  defp update_users_fax_preference(user_preferences, params) do
-    if Map.has_key?(params, :likes_faxes) do
-      %{likes_faxes: likes_faxes} = params
-      Map.merge(user_preferences, %{likes_faxes: likes_faxes})
-    else
-      user_preferences
-    end
-  end
-
-  defp add_user_id_for_subscription(user_preferences, user) do
-    {:ok ,Map.merge(user_preferences, %{user_id: user.id})}
   end
 end
